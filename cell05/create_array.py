46671225136b61a45421e5ae3db846a6fa572e23
Payuth Charoensri <61724549+punpayut@users.noreply.github.com>
2024-10-09 15:26:27 +0700

    Add files via upload

---
+++ cell05/create_array.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Main program
+if __name__ == "__main__":
+    # Define an array of numbers
+    numbers = [2, 8, 9, 48, 8, 22, -12, 2]
+    
+    # Display the array on the screen
+    print(numbers)
