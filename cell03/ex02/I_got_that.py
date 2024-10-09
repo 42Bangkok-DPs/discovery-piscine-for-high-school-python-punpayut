deba5191ebac97dab8f9da77db7885504ac7c761
Payuth Charoensri <61724549+punpayut@users.noreply.github.com>
2024-10-09 11:10:19 +0700

    Add files via upload

---
+++ cell03/ex02/I_got_that.py
@@ -0,0 +1,14 @@
+# Main program
+if __name__ == "__main__":
+    # First input prompt
+    user_input = input("What you gotta say? : ")
+    
+    # Loop until the user enters "STOP"
+    while user_input != "STOP":
+        print("I got that! Anything else? : ", end="")
+        # Continue asking for input
+        user_input = input()
+
+
+
+
